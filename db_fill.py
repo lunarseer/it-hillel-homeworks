689967d0c29f74345370bb30cb218a92f7350125
Igor <igor.chopenko@gmail.com>
2021-06-28 12:46:33 +0300

    hotfix db_fill.py

--- db_fill.py
+++ db_fill.py
@@ -15,8 +15,8 @@ db.create_all()
 
 def add_Sales():
     with open('homework3sales.csv', mode='r') as csvfile:
-        csvdata = [x for x in csv.reader(csvfile[1:], delimiter=';')] #Removing first row with fields names
-        for i, row in enumerate(csvdata):                       
+        csvdata = [x for x in csv.reader(csvfile, delimiter=';')]
+        for i, row in enumerate(csvdata[1:]):                       #Removing first row with fields names   
             sale = Sales(Transaction_date=row[0],
                         Product=row[1],
                         Price=row[2],
