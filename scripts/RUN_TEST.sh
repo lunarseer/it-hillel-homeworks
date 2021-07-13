0e742fda39bc63f288f21218eaa5e3915799978d
igor.chopenko <igor.chopenko@gmail.com>
2021-07-13 15:36:42 +0300

    up

--- scripts/RUN_TEST.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-pip install -r ../requirements.txt
-export DJANGO_SUPERUSER_PASSWORD=admin
-python ../mysite/manage.py createsuperuser --noinput --user admin --email mail@mail.com
-python ../mysite/manage.py makemigrations
-python ../mysite/manage.py migrate
